016bc22fc2459f1b9cd6d77162d30a6a630577b2
IT-yuancai <m15755116732@163.com>
2019-05-21 14:19:38 +0800

    xiugaitjiao

--- ant_test/src/com/yuan/test/AntTest.java
+++ ant_test/src/com/yuan/test/AntTest.java
@@ -1,10 +1,13 @@
 package com.yuan.test;
 
+
 public class AntTest {
 
 	public static void main(String[] args) {
 		System.out.println("this is a git project.");
 		System.out.println("this is second git project.");
 		System.out.println("this is fourth git project.");
+		String app = "app";
+		System.out.println(app);
 	}
 }
