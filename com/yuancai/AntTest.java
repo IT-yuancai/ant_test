a36010db317c295e67ae3e03412a114b9186b5b8
m15755116732@163.com <m15755116732@163.com>
2019-12-25 22:23:27 +0800

    this is new branch first commit

---
+++ com/yuancai/AntTest.java
@@ -0,0 +1,21 @@
+package com.yuan.test;
+
+
+public class AntTest {
+
+	public static void main(String[] args) {
+		System.out.println("this is a git project.");
+		System.out.println("this is second git project.");
+		System.out.println("this is fourth git project.");
+		String app = "app";
+		System.out.println(app);
+	}
+	
+        public static void set() {
+               System.out.println("this is a set funtion");
+        }
+
+	public static void get() {
+		System.out.println("this is a get funtion");
+	}
+}
